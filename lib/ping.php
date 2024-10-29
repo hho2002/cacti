341f58457f27e07fd037917128fa6f04edd37d3d
Petr Macek <petr.macek@kostax.cz>
2024-10-29 18:17:05 +0100

    fix ipv6 params (#5883)
    * fix ipv6 params
    
    * add path check
    
    * Update ping.php to fix syntax error
    
    ---------
    
    Co-authored-by: TheWitness <thewitness@cacti.net>

--- lib/ping.php
+++ lib/ping.php
@@ -195,7 +195,7 @@ class Net_Ping {
 					$result = shell_exec('ping -t ' . ceil($this->timeout / 1000) . ' -c ' . $this->retries . ' ' . $this->host['hostname']);
 				} elseif (substr_count(strtolower(PHP_OS), 'freebsd')) {
 					if (strpos($this->host['hostname'], ':') !== false) {
-						$result = shell_exec('/usr/sbin/ping6 -X ' . ceil($this->timeout / 1000) . ' -c ' . $this->retries . ' ' . $this->host['hostname']);
+						$result = shell_exec('/usr/sbin/ping6 -t ' . ceil($this->timeout / 1000) . ' -c ' . $this->retries . ' ' . $this->host['hostname']);
 					} else {
 						$result = shell_exec('ping -t ' . ceil($this->timeout / 1000) . ' -c ' . $this->retries . ' ' . $this->host['hostname']);
 					}
@@ -217,9 +217,16 @@ class Net_Ping {
 					} else {
 						$result = shell_exec('ping -W ' . ceil($this->timeout / 1000) . ' -c ' . $this->retries . ' -p ' . $pattern . ' ' . $this->host['hostname'] . ' 2>&1');
 
-						if ((strpos($result, 'unknown host') !== false || strpos($result, 'Address family') !== false) && file_exists('/usr/sbin/ping6')) {
-							$result = shell_exec('/usr/sbin/ping6 -W ' . ceil($this->timeout / 1000) . ' -c ' . $this->retries . ' -p ' . $pattern . ' ' . $this->host['hostname']);
+					if (strpos($result, 'unknown host') !== false || strpos($result, 'Address family') !== false) {
+						if (file_exists('/usr/bin/ping6')) {
+							$ping_path = '/usr/bin/ping6';
+						} elseif (file_exists('/usr/sbin/ping6')) {
+							$ping_path = '/usr/sbin/ping6';
+						} else {
+							$ping_path = '/bin/ping6';
 						}
+
+						$result = shell_exec($ping_path . ' -W ' . ceil($this->timeout/1000) . ' -c ' . $this->retries . ' -p ' . $pattern . ' ' . $this->host['hostname']);
 					}
 				}
 			}
